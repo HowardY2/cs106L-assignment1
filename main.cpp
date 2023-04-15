abc428788c688dbd3fa05b83800747f347fddfc6
HowardY <howardy2020@gmail.com>
2023-04-15 11:21:49 +0800

    finish

--- main.cpp
+++ main.cpp
@@ -1,25 +1,32 @@
-#include <iostream>     	// for cout, cin
-#include <fstream>      	// for ifstream
-#include <sstream>      	// for stringstream
-#include <filesystem>   	// making inputting files easier
-#include <unordered_set>	// containers for the wikiscraper
+#include <iostream>      // for cout, cin
+#include <fstream>       // for ifstream
+#include <sstream>       // for stringstream
+#include <filesystem>    // making inputting files easier
+#include <unordered_set> // containers for the wikiscraper
 #include <vector>
 #include <queue>
 #include <unordered_map>
-#include "wikiscraper.h"    // wikiscraper methods
+#include "wikiscraper.h" // wikiscraper methods
 
-using std::cout;            using std::endl;
-using std::ifstream;        using std::stringstream;
-using std::string;          using std::vector;
-using std::priority_queue;  using std::unordered_map;
-using std::unordered_set;   using std::cin;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::priority_queue;
+using std::string;
+using std::stringstream;
+using std::unordered_map;
+using std::unordered_set;
+using std::vector;
 
-int main() {
+int main()
+{
     // a quick working directory fix to allow for easier filename inputs
     auto path = std::filesystem::current_path() / "res/";
     std::filesystem::current_path(path);
     std::string filenames = "Available input files: ";
-    for (const auto& entry : std::filesystem::directory_iterator(path)) {
+    for (const auto &entry : std::filesystem::directory_iterator(path))
+    {
         std::string filename = entry.path().string();
         filename = filename.substr(filename.rfind("/") + 1);
         filenames += filename + ", ";
@@ -34,23 +41,41 @@ int main() {
     string filename;
     getline(cin, filename);
 
-    /* 
+    /*
         TODO: Create a filestream from the filename.
         For each pair {start_page, end_page} in the input file,
         retrieve the result of findWikiLadder(start_page, end_page)
         and append that vector to outputLadders.
     */
     // Write code here
+    ifstream input(filename);
+
+    int lines;
+    string start_page, end_page;
+
+    input >> lines;
+    cout << lines << endl;
+    for (int i = 0; i < lines; ++i)
+    {
+        input>>start_page>>end_page;
+        cout << start_page << " " << end_page<< endl;
+        outputLadders.push_back(findWikiLadder(start_page, end_page));
+    }
 
     /*
      * Print out all ladders in outputLadders.
      * We've already implemented this for you!
      */
     // Write code here
+    for(auto ladder : outputLadders)
+    {
+        for(auto it = ladder.begin(); it != ladder.end(); it++)
+        {
+            string page = *it;
+            cout << page << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
-
-
-
-
